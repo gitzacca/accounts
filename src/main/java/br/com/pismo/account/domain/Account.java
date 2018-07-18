12ba0ed036b67eba7630d8481e170d74b84a1fe4
Bruno Zaccaria <zaccabruno@gmail.com>
2018-07-18 16:45:50 -0300

    first commit

---
+++ src/main/java/br/com/pismo/account/domain/Account.java
@@ -0,0 +1,13 @@
+package br.com.pismo.account.domain;
+
+import java.math.BigDecimal;
+
+public class Account {
+
+    private Long id;
+    private BigDecimal availableCreditLimit;
+    private BigDecimal availableWithdrawalLimit;
+
+
+
+}
